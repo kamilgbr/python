26713b8f3cc2870dbe01a62655712e7f3094ae42
kamilgbr <gabrysiak22@gmail.com>
2022-05-31 12:44:55 +0200

    pushing ended project

---
+++ main.py
@@ -0,0 +1,24 @@
+def decoder():
+    f = open("test.txt", 'r')
+    lines = f.readlines()
+    index = 5
+
+    for x in lines:
+        for char in x.strip():
+            if char == "R":
+                if index % 3 != 0:
+                    index += 1
+            elif char == "U":
+                if index >= 4:
+                    index -= 3
+            elif char == "D":
+                if index < 7:
+                    index += 3
+            elif char == "L":
+                if index not in [1, 4, 7]:
+                    index -= 1
+
+        print("klucz: ", index)
+
+
+decoder()
